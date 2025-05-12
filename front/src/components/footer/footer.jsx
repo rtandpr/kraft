b6a65ab32d10221559ca9b5faf8be427fd07b58b
rtandpr <alejandromocse01@gmail.com>
2025-05-12 19:50:42 -0300

    V41

--- front/src/components/footer/footer.jsx
+++ front/src/components/footer/footer.jsx
@@ -18,12 +18,14 @@ const Footer = () => {
           <br className={styles.brResp} />
           <Link to="/Priv">AVISO DE PRIVACIDAD</Link>
         </div>
-        <span className={styles.promotion}>
-          "Sorteo termina el 31 de julio de 2025. Puede participar toda persona mayor de edad, residente legal de PR. No es necesario comprar para participar y ganar.
+        <div className={styles.textjUST}>
+
+       
+        <span className={styles.promotion}>"Sorteo termina el 31 de julio de 2025. Puede participar toda persona mayor de edad, residente legal de PR. No es necesario comprar para participar y ganar.
           <br />
           Promotor: Kraft Foods Group Puerto Rico, LLC. Reglas oficiales en sabeakraftheinz.com
         </span>
-
+ </div>
 
         <LogosRedes></LogosRedes>
 
